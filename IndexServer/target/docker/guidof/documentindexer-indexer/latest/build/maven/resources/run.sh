a185536d02dd1b51a0ed1776c3bed93ee19bdd87
guido4f <james.fawke@g4pas.co.uk>
2018-12-17 13:56:35 +0000

    initial commit -  cleanup

--- IndexServer/target/docker/guidof/documentindexer-indexer/latest/build/maven/resources/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /app
-applicationYaml=$(RABBITMQ_PASSWORD=$(cat ${RABBITMQ_PASSWORD_FILE}) /usr/bin/envsubst < application.yaml)
-echo "${applicationYaml}" > application.yaml
-echo "Waiting 60sec for RabbitMq to be up"
-sleep 60
-java -Djava.security.egd=file:/dev/./urandom -jar /app/IndexServer.jar
